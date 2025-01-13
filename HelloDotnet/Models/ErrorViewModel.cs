677284b4c7af0f7c696249dedbf5e5eaf4c65f39
Max Peach <maxpeach742@gmail.com>
2025-01-13 15:22:51 -0400

    Add project files.

---
+++ HelloDotnet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HelloDotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
